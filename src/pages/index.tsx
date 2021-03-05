c21c4ae3c1bd3af32bb406e732c021c906ecade0
Andre Robette <andreluizrobette19@gmail.com>
2021-03-05 16:06:03 -0300

    Landing Page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,14 @@
-import Head from "next/head";
 import LandingPage from "./LandingPage";
+
+import Head from "next/head";
 
 export default function Home() {
   return (
     <div>
       <Head>
-        <link rel="icon" href="icon.png" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Chakra+Petch:wght@300;400;500;600;700&family=Rajdhani:wght@300;400;500;600;700&display=swap"
-          rel="stylesheet"
-        />
         <title>useMEMOry</title>
       </Head>
-      <LandingPage/>
+      <LandingPage />
     </div>
   );
 }
