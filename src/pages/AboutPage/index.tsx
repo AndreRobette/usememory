525d392aebfb05119ec0267187b68947c107e810
Andre Robette <andreluizrobette19@gmail.com>
2021-03-06 00:40:37 -0300

    AboutPage

--- src/pages/AboutPage/index.tsx
+++ src/pages/AboutPage/index.tsx
@@ -1,11 +1,67 @@
-import React from 'react';
+import React from "react";
 
-import { Container } from './styles';
+import {
+  CollaboratorProfile,
+  Collaborators,
+  CollaboratorsContainer,
+  Container,
+  MainText,
+} from "./styles";
 
 const AboutPage: React.FC = () => {
   return (
     <Container>
-      <h1>AboutPage</h1>
+      <h1>
+        O que é o <span></span>
+        <span>use</span>
+        <span>MEMO</span>
+        <span>ry</span>
+      </h1>
+      <MainText>
+        Nosso projeto consiste em desenvolver um jogo da memória para pessoas
+        com deficiência intelectual
+      </MainText>
+      <CollaboratorsContainer>
+        <h1>Colaboradores</h1>
+        <hr />
+        <Collaborators>
+          <CollaboratorProfile
+            href="https://github.com/AndreRobette"
+            target="_blank"
+          >
+            <img src="/images/Andre.png" alt="André Robette" />
+            <h2>André Robette</h2>
+          </CollaboratorProfile>
+          <CollaboratorProfile
+            href="https://github.com/PietroSouza"
+            target="_blank"
+          >
+            <img src="/images/Pietro.png" alt="André Robette" />
+            <h2>Gabriel P. de Souza</h2>
+          </CollaboratorProfile>
+          <CollaboratorProfile
+            href="https://github.com/julyabm"
+            target="_blank"
+          >
+            <img src="/images/Julya.png" alt="André Robette" />
+            <h2>Julya Marssona</h2>
+          </CollaboratorProfile>
+          <CollaboratorProfile
+            href="https://github.com/LucasJantschChitolina"
+            target="_blank"
+          >
+            <img src="/images/Lucas.png" alt="André Robette" />
+            <h2>Lucas Chitolina</h2>
+          </CollaboratorProfile>
+          <CollaboratorProfile
+            href="https://github.com/AndreRobette"
+            target="_blank"
+          >
+            <img src="/images/Putti.png" alt="André Robette" />
+            <h2>Mateus J. Putti</h2>
+          </CollaboratorProfile>
+        </Collaborators>
+      </CollaboratorsContainer>
     </Container>
   );
 };
--- src/pages/AboutPage/styles.ts
+++ src/pages/AboutPage/styles.ts
@@ -1,5 +1,105 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
 
+  width: 100%;
+  height: 100vh;
+
+  > h1 {
+    font-size: 4.5rem;
+    font-weight: 500;
+    text-shadow: 0px 5px 5px rgba(0, 0, 0, 0.25);
+  }
+
+  > h1 > span:nth-child(2) {
+    color: var(--green);
+  }
+
+  > h1 > span {
+    color: var(--brown);
+    transition: 0.2s;
+
+    :hover {
+      transform: scale(1.1);
+      filter: brightness(1.3);
+    }
+  }
+
+  > h1 > span:nth-child(4) {
+    color: var(--text);
+  }
+`;
+
+export const MainText = styled.h2`
+  max-width: 39.375rem;
+  font-size: 3rem;
+  font-weight: 500;
+
+  text-align: center;
+`;
+
+export const CollaboratorsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  max-width: 55.313rem;
+  width: 100%;
+  align-items: center;
+
+  > h1 {
+    font-size: 3rem;
+    font-weight: 500;
+    transition: 0.2s;
+    cursor: default;
+
+    :hover {
+      filter: brightness(1.3);
+    }
+  }
+
+  > hr {
+    width: 100%;
+    margin-bottom: 35px;
+  }
+`;
+
+export const Collaborators = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+`;
+
+export const CollaboratorProfile = styled.a`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+
+  cursor: pointer;
+
+  max-width: 160px;
+  width: 100%;
+
+  transition: 0.2s;
+
+  > img {
+    max-width: 140px;
+  }
+
+  > h2 {
+    font-size: 1.1rem;
+  }
+
+  :hover {
+    transform: scale(1.1);
+
+    > h2 {
+        filter: brightness(1.3);
+    }
+  }
 `;
