8b59440e3180e71d906525488d9d130bc82e5a43
Andre Robette <andreluizrobette19@gmail.com>
2021-03-12 18:33:23 -0300

    animal icons imported and cards made

--- src/components/Game/styles.ts
+++ src/components/Game/styles.ts
@@ -79,7 +79,7 @@ const CardBack = styled.div`
         text-shadow: 0px 5px 5px rgba(0, 0, 0, 0.25);
         justify-content: flex-end;
         align-items: center;
-        line-height: 25px;
+        line-height: 20px;
     }
 `;
 
