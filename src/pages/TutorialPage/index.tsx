2d9dfd2eed1290f8ef9afaae01ef0bb3ab554b2e
Lucas Chitolina <lucas.chitolina01@gmail.com>
2021-04-15 10:01:49 -0300

    tutorial page done

--- src/pages/TutorialPage/index.tsx
+++ src/pages/TutorialPage/index.tsx
@@ -1,17 +1,18 @@
-import Head from 'next/head';
 import React from 'react';
-import MainHeader from '../../components/MainHeader';
+import Link from "next/link";
 
-import { Container } from './styles';
+import { Container, Header, Icon } from './styles';
 
 function TutorialPage({ children }) {
   return (
     <Container>
-      <Head>
-        <title>useMEMOry</title>
-      </Head>
-      <MainHeader />
-      <h1>TutorialPage</h1>
+      <Header>
+        <Link href="/LandingPage">
+          <Icon src="/images/headerIcon.svg" alt="usememory" />
+        </Link>
+        <Link href="/LandingPage">Retornar</Link>
+      </Header>
+      <iframe src="https://www.youtube.com/embed/fk0aWk2yNQk" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
     </Container>
   );
 };
