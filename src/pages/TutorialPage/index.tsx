f77b5dd3c0e522364bc80718df1d0930e337bc20
Lucas Chitolina <lucas.chitolina01@gmail.com>
2021-04-15 07:51:39 -0300

    local commit

--- src/pages/TutorialPage/index.tsx
+++ src/pages/TutorialPage/index.tsx
@@ -1,12 +1,54 @@
 import React from 'react';
 
-import { Container } from './styles';
+import { Grid } from './styles';
 
-const TutorialPage: React.FC = () => {
+import MainHeader from '../../components/MainHeader';
+import Aside from '../../components/Aside';
+import Game from '../../components/Game';
+import { LevelingProvider } from '../../contexts/LevelingContext';
+import { Card } from '../../components/Game/styles';
+
+import {
+  GiFeline as Cat,
+  GiPig as Pig,
+} from "react-icons/gi";
+
+const cardBack = '/images/cardBack.svg'
+
+function showCards() {
   return (
-    <Container>
-      <h1>TutorialPage</h1>
-    </Container>
+    <>
+      <Card id="card" className="memory-card" data-framework="cat" >
+        <Cat size={140} className="front-face" />
+        <img src={cardBack} alt="cardBack" className="back-face" />
+      </Card>
+      <Card id="card" className="memory-card" data-framework="cat" >
+        <Cat size={140} className="front-face" />
+        <img src={cardBack} alt="cardBack" className="back-face" />
+      </Card>
+      <Card id="card" className="memory-card" data-framework="pig" >
+        <Pig size={140} className="front-face" />
+        <img src={cardBack} alt="cardBack" className="back-face" />
+      </Card>
+      <Card id="card" className="memory-card" data-framework="pig" >
+        <Pig size={140} className="front-face" />
+        <img src={cardBack} alt="cardBack" className="back-face" />
+      </Card>
+    </>
+  );
+}
+
+function TutorialPage({ children }) {
+  return (
+    <LevelingProvider>
+      <Grid>
+        <MainHeader />
+        <Aside />
+        <Game>
+          {children}
+        </Game>
+      </Grid>
+    </LevelingProvider>
   );
 };
 
--- src/pages/TutorialPage/styles.ts
+++ src/pages/TutorialPage/styles.ts
@@ -1,5 +1,19 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
+export const Grid = styled.div`
+    display: grid;
+    grid-template-columns: 75% auto;
+    grid-template-rows: 130px auto;
+    
+    grid-template-areas:
+    'MH AS'
+    'CT AS';
 
+    height: 100vh;
 `;
+
+
+
+
+
+
