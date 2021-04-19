2a324d629e5f4b66d4a4aed8e7414dc53f279384
Gabriel Pietro de Souza <71415416+PietroSouza@users.noreply.github.com>
2021-04-19 10:27:35 -0300

    Update styles.ts

--- src/pages/AboutPage/styles.ts
+++ src/pages/AboutPage/styles.ts
@@ -1,5 +1,9 @@
 import styled from "styled-components";
 
+export default () => {
+    return "Show page working ! ";
+}
+
 export const Container = styled.div`
     display: flex;
     flex-direction: column;
